11d1f455f44cc7170b7b6f7b6c5f1d16d74c4760
Bwana Kizito <kizomanizo@gmail.com>
2020-04-10 20:26:05 +0300

    Remote Destination

--- controllers/message.js
+++ controllers/message.js
@@ -6,8 +6,8 @@ const net = require('net');
 const VT = String.fromCharCode(0x0b);
 const FS = String.fromCharCode(0x1c);
 const CR = String.fromCharCode(0x0d);
-// const remoteOptions = {host: '154.72.82.199', port: 2200};
-const remoteOptions = {host: '127.0.0.1', port: 60920};
+const remoteOptions = {host: '154.72.82.199', port: 2200};
+// const remoteOptions = {host: '127.0.0.1', port: 60920};
 var client = require('./client');
 
 module.exports = {
